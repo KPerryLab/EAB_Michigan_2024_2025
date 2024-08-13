6bce2d05fc64cf423286d96c31c05546eeed1640
Aaron Tayal <tayal.14@osu.edu>
2024-08-13 19:41:56 -0400

    Enter data for EAB Michigan
    I entered all the data for the plot centers.

--- Aug_2024_poster/ash_occurence_for_Aug_2024_poster.R
+++ Aug_2024_poster/ash_occurence_for_Aug_2024_poster.R
@@ -6,6 +6,8 @@
 library(ggplot2)
 library(dplyr)
 
+# Seedlings -------------------------------------------------------------------
+
 seedlings <- read.csv("Raw_data/07_08_2024_ash_seedlings.csv")
 seedlings$center_tree_number <- as.integer(seedlings$center_tree_number)
 hydro <- read.csv("Raw_data/MI-plot-hydroclasses.csv")
@@ -64,6 +66,44 @@ ggplot(data=seedlings2, aes(x=factor(mstrlvl),
   geom_jitter(height=0, width=0.1, alpha=0.5) +
   xlab("Hydroclass") +
   ylab(bquote("Density of ash seedlings " ~ (stems/m^2)))
+
+# I want to take an average of the 4 microplots in order to report data at the 
+# plot level.
+seedlings_by_plot <- seedlings2 %>% group_by(center_tree_number) %>% 
+  summarize(mean_perc_cov = mean(percent_cover_0_0.5_1_3.5_8_15.5_25.5_etc),
+            mean_density_est_seedl = mean(density_established_seedlings_stems_per_m_squared),
+            mstrlvl = first(mstrlvl))
+
+# Make a graph that shows the seedling densities by hydroclass, at the plot level
+ggplot(data=seedlings_by_plot, aes(x=factor(mstrlvl), 
+                            y=mean_density_est_seedl)) +
+  geom_violin() +
+  geom_jitter(height=0, width=0.1, alpha=0.5) +
+  xlab("Hydroclass") +
+  ylab(bquote("Density of ash seedlings " ~ (stems/m^2)))
+
+# -----------------------------------------------------------------------------
+# Saplings --------------------------------------------------------------------
+
+saplings <- read.csv("Raw_data/07_09_2024_ash_saplings.csv")
+saplings$center_tree_number <- as.integer(saplings$center_tree_number)
+
+# Trying to add in the hydroclass to this dataframe:
+saplings2 <- inner_join(saplings, hydro, by="center_tree_number")
+
+# Saplings were counted in each quadrant, so I want to sum up the quadrants
+# to get a total number of saplings by plot
+saplings_by_plot <- saplings2 %>% group_by(center_tree_number) %>% 
+  summarize(number_saplings = sum(number_of_stems),
+            mstrlvl = first(mstrlvl))
+
+
+
+
+
+
+
+
 
   
 
