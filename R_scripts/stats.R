a8e1c187c27b1a3ffd2595710c07011fabe2ea27
Kayla I Perry <kiperry1488@gmail.com>
2024-10-14 12:31:51 -0400

    added code for multiple comparisons

--- R_scripts/stats.R
+++ R_scripts/stats.R
@@ -7,14 +7,25 @@ library(ggplot2)
 library(dplyr)
 library(lme4)
 
+library(car)
+library(emmeans)
+
 seedlings_by_transect <- read.csv("Cleaned_data/seedlings_by_transect.csv")
 seedlings_by_transect$Transect <- as.factor(seedlings_by_transect$Transect)
 seedlings_by_transect$Park <- as.factor(seedlings_by_transect$Park)
 seedlings_by_transect$mstrlvl <- as.factor(seedlings_by_transect$mstrlvl)
 
+dotchart(seedlings_by_transect$total_number_seedlings, group = seedlings_by_transect$mstrlvl)
+hist(seedlings_by_transect$total_number_seedlings)
+boxplot(seedlings_by_transect$total_number_seedlings ~ seedlings_by_transect$mstrlvl)
+stripchart(total_number_seedlings ~ mstrlvl, data = seedlings_by_transect, col = c("black"), vertical = TRUE,
+           pch = 19, cex = 2, add = TRUE, method = "jitter", jitter = 0.2)
 
-
-glmer(total_number_seedlings ~ mstrlvl + (1|Park), data=seedlings_by_transect,
+seedling_mod <- glmer(total_number_seedlings ~ mstrlvl + (1|Park), data=seedlings_by_transect,
       family="poisson")
+summary(seedling_mod)
+Anova(seedling_mod)
+emmeans(seedling_mod, pairwise ~ mstrlvl)
 
-
+qqnorm(resid(seedling_mod))
+qqline(resid(seedling_mod))
